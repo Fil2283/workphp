aa2c7a2770c0d625f643b1e6fa581006574d8fe9
Philipp Goenko <7171317@mail.ru>
2023-08-08 12:11:25 +0300

    3

--- task_3.php
+++ task_3.php
@@ -1,9 +1,10 @@
 <?php
-function Time($t) {
+function Time($t)
+{
     $PM = substr($t, -2) === "PM";
-    
+
     $t = str_replace(["AM", "PM"], "", $t);
-    
+
     list($h, $m, $s) = explode(":", $t);
 
     if ($PM && $h !== "12") {
@@ -17,5 +18,3 @@ function Time($t) {
 $t = "03:30:45 PM";
 $t = Time($t);
 echo $t; // Вывод: "15:30:45"
-
-?>
